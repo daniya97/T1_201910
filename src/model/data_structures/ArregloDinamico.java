7f88bb99e329add39d2d4d4a4f03676d0f8f770b
Luis Eduardo Yamin Lacouture <lyamin@uniandes.edu.co>
2019-01-30 08:49:47 -0500

    Arreglo Dinámico para Strings terminado

--- src/model/data_structures/ArregloDinamico.java
+++ src/model/data_structures/ArregloDinamico.java
@@ -50,26 +50,34 @@ public class ArregloDinamico implements IArregloDinamico {
        }
 
 		public int darTamano() {
+			
+			//Devuelve le tamaño actual guardado como atributo
 			return tamanoAct;
 		}
 
 		public String darElemento(int i) {
 			// TODO implementar
 			
+			//Si la posición es mayor al tamaño actual se devuelve null
 			if (i>=tamanoAct) {
 				return null;
 			}
 			
+			
+			//Si no se devuelve el elemento
 			return elementos[i];
 			
 		}
 
 		public String buscar(String dato) {
-
+			
+			
+			//Boolean para identificar si ya fue encontrado el dato o no
 			Boolean encontrado = false;
 			
 			for (int i = 0; i < tamanoAct && !encontrado; i++) 
 			{
+				//Se compara con equals pues lo implementa cualquier objeto
 				if (dato.equals(elementos[i])) {
 					encontrado = true;
 				}
@@ -78,6 +86,7 @@ public class ArregloDinamico implements IArregloDinamico {
 			
 			
 			if (encontrado) {
+				//Si se encontró se devuelve el objeto sino se devuelve null
 				return dato;
 			}
 			return null;
@@ -85,12 +94,16 @@ public class ArregloDinamico implements IArregloDinamico {
 
 		public String eliminar(String dato) {
 		
+			
+			//Se crea un arreglo auxiliar para guardar los Strings que no se eliminan
 			String [] auxiliar = new String[tamanoAct];
 			boolean encontrado = false;
 			int tamanoAuxiliar = 0;
 			
 			int contador = 0;
+		
 			
+		//Se recorre el arreglo original transferiendo los String al auxiliar excepto que sea el dato en cuestión
 		for (int i = 0; i <tamanoAct; i++) {
 				if(!dato.equals(elementos[i])){
 					auxiliar[contador] = elementos[i];
@@ -103,10 +116,16 @@ public class ArregloDinamico implements IArregloDinamico {
 				}
 		}
 			
+		
+		//si nunca se encontró se devuelve null
 		if(!encontrado) return null;
+		
+		//Se encuentra el número total de datos eliminados
 		int numeroEliminados = tamanoAct - tamanoAuxiliar;
 		
 		
+		
+		//Caso de que se hayan eliminado todos los elementos
 		if(numeroEliminados == tamanoAct){
 			elementos  = new String[1];
 			tamanoAct = 0;
@@ -115,6 +134,7 @@ public class ArregloDinamico implements IArregloDinamico {
 		}
 		
 		
+		//Caso restante: Queda el arreglo auxiliar como el arreglo elementos
 		tamanoAct = tamanoAuxiliar;
 		while(tamanoAuxiliar <= tamanoMax/2){
 			tamanoMax = tamanoMax/2;
@@ -128,6 +148,8 @@ public class ArregloDinamico implements IArregloDinamico {
 			elementos[i] = auxiliar[i];
 			}
 		
+		
+		//Se devuelve el dato eliminado
 		return dato;	
 	}
 }
